dcc4952f195534e4ff4bdaeea1e3119d4be60c62
Elias K <127874391+n2koel00@users.noreply.github.com>
2023-05-07 17:48:49 +0300

    Nimi textarea lisätty

--- Koodit/code.js
+++ Koodit/code.js
@@ -4,22 +4,22 @@ let body = document.querySelector("body");
 
 let note = document.getElementById("viesti");
 let checkbox = document.getElementById("important");
+let name = document.getElementById("name");
 
 
 function addNote(){
 
-    let h3 = document.createElement("h3");
-
+    let h5 = document.createElement("h5");
+    h5.textContent = name.value;
     let d = new Date();
     let b = document.createElement("b");
     b.textContent = viesti.value;
     
     //onko checkboxissa täppi vai ei 
     if(checkbox.checked){
-        h3.classList.add("important");
         b.style.borderColor = "red";
     }
     //kaikki elementit bodyn sisälle
-    body.append(h3, b, d, document.createElement("hr"));
+    body.append(h5, b, d, document.createElement("hr"));
 
 }
